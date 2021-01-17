f1a6798eeb8c14b249b43cf9b2b6fbf37189e00e
Diana Liao <dijali@gmail.com>
2021-01-17 22:42:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,8 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   my_hash = {
-    dinner: pizza,
-    activity: movie
+    dinner: "pizza",
+    activity: "movie"
   }
 end
 
